d57bfb579e73278b0ecc6f6b0fb0ee1907b6fdfd
wiame <kazdari.wiam@gmail.com>
2018-06-06 21:31:37 +0000

    firsst commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './App.css'
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+class App extends Component {
+  handleCardClick(card){
+    console.log(card,'clicked')
+  }
+  render() {
+    const won = new Date().getSeconds() % 2 ===0
+    return(
+      <div className="memory">
+          <GuessCount guesses= {0} />
+          <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+          <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+          <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+          <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+          <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+          <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+          {won && <p> GANGE ! </p> }
+      </div>
+    )
+  }
+}
+
+export default App
